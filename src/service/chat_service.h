5b396e04e1c1f3bc8cd1ed79a982215aa6fde599
陈志仪 <ggbond.czy@gmail.com>
2025-12-01 17:21:52 +0800

    feat:增加未读消息提示功能

--- src/service/chat_service.h
+++ src/service/chat_service.h
@@ -3,6 +3,8 @@
 
 #include <QObject>
 #include <QString>
+#include <QMap>
+#include <QMutex>
 #include "common/types.h"
 
 class ChatService : public QObject
@@ -17,17 +19,27 @@ public:
     
     void sendMessage(const LanChat::Message& message);
     void receiveMessage(const LanChat::Message& message);
-    
+
+    // 未读计数 API
+    void setActiveChatUserId(const QString& userId);
+    void markSessionRead(const QString& userId);
+    int getUnreadCount(const QString& userId);
+
 signals:
     void messageSent(const LanChat::Message& message);
     void messageReceived(const LanChat::Message& message);
+    void unreadCountChanged(const QString& userId, int count);
     void errorOccurred(const QString& error);
 
 private:
-    ChatService() = default;
+    ChatService();
     ~ChatService() = default;
     ChatService(const ChatService&) = delete;
     ChatService& operator=(const ChatService&) = delete;
+
+    QMap<QString,int> m_unreadMap; // 内存中的未读计数缓存
+    QString m_activeChatUserId;
+    QMutex m_mutex;
 };
 
 #endif // CHAT_SERVICE_H
